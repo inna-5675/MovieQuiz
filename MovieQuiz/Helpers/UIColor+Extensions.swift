616d9501ee1bde38ee950e3aa145adb3716bf2e5
Butkova Inna <inna-5675@mail.ru>
2025-02-08 14:05:38 +0500

    Изменения по пятому спринту

--- MovieQuiz/Helpers/UIColor+Extensions.swift
+++ MovieQuiz/Helpers/UIColor+Extensions.swift
@@ -1,10 +1,5 @@
 import UIKit
 
 extension UIColor {
-    static var ypGreen: UIColor {UIColor(named: "YP Grren") ?? UIColor.green}
-    static var ypRed: UIColor {UIColor(named: "YP Red") ?? UIColor.red}
-    static var ypBlack: UIColor { UIColor(named: "YP Black") ?? UIColor.black }
-    static var ypBackground: UIColor {UIColor(named: "YP Background") ?? UIColor.darkGray}
-    static var ypGray: UIColor {UIColor(named: "YP Gray") ?? UIColor.gray}
-    static var ypWhite: UIColor {UIColor(named: "YP Wite") ?? UIColor.white}
+   
 }
